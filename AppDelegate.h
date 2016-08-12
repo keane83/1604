8a216b95bb7bb79bf0533ad7d6d201ab8fb0f6a6
keane <liujian830527@126.com>
2016-08-12 10:46:49 +0800

    Add

---
+++ AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  iFreeDemo
+//
+//  Created by keane on 16/8/9.
+//  Copyright © 2016年 keane. All rights reserved.
+//111111111111111
+//123123123123123
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
